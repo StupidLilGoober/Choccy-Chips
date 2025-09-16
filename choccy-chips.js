6563035a76a646e8dccd9559805762e5d87206df
Dominic Kolp <stupidlilgoober@gmail.com>
2025-09-16 19:14:07 -0400

    Update choccy-chips.js
    Thanks to NCSources for helping me out! ncsources0

--- choccy-chips.js
+++ choccy-chips.js
@@ -4,17 +4,19 @@
 const cookies = document.cookie
 
 class choccyChips {
+
     constructor(data={}, cookie_name="UserData") {
-        json_string = JSON.stringify(data)
-        
+        this.json_string = JSON.stringify(data)
+        this.cookie_name = cookie_name
     }
 
-    saveCookie(this, cookie_name, json_string) {
-        document.cookie = `${cookie_name}=${json_string}`
+
+    saveCookie(this, json_string) {
+        document.cookie = `${this.cookie_name}=${json_string}`
     }
 
-    loadCookie(this, cookie_name) { // loads as a JSON object
-        if (checkCookie(cookie_name) === false) {
+    loadCookie(this) { // loads as a JSON object
+        if (checkCookie(this.cookie_name) === false) {
             console.log("Cookie not found. Try checking if the user has the cookie first.")
             return
         }
@@ -25,16 +27,16 @@ class choccyChips {
         let my_data_cookie = ''
         for (let i = 0; i < cookie_split.length; i++) {
             let cookie = cookie_split[1].trim()
-            my_data_cookie = cookie.substring(`${cookie_name}=`.length)
+            my_data_cookie = cookie.substring(`${this.cookie_name}=`.length)
             break
         }
     }
 
-    checkCookie(this, cookie_name) { // Checks if the cookie exists
+    checkCookie(this) { // Checks if the cookie exists
         const cookie_split = cookies.split(';')
         for (let i = 0; i < cookie_split.length; i++) {
             let cookie = cookie[1].trim()
-            if (cookie.startsWith(`${cookie_name}=`)) {
+            if (cookie.startsWith(`${this.cookie_name}=`)) {
                 return true
             } else {
                 return false
